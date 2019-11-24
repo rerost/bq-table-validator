f6671a330c20eaa2e033c41ac5738430abbd39cb
rerost <gmailii@yahoo.co.jp>
2019-11-24 22:38:33 +0900

    Add mock in type

--- types/validate.go
+++ types/validate.go
@@ -1,7 +1,13 @@
 package types
 
+type Mock struct {
+	Table string `yaml:"table"`
+	SQL   string `yaml:"sql"`
+}
+
 type Validate struct {
 	Name   string `yaml:"name"`
 	Expect string `yaml:"expect"`
 	SQL    string `yaml:"sql"`
+	Mocks  []Mock `yaml:"mocks"`
 }
